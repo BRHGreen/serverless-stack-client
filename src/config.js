dec061b79bab917bc180c26399e552a778067691
Benedict Green <benedictgreen@benedicts-mbp.mynet>
2020-01-02 23:27:06 +0000

    Client set up

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    REGION: "eu-west-2",
+    BUCKET: "notes-app-uploads-bg"
+  },
+  apiGateway: {
+    REGION: "eu-west-2",
+    URL: "https://4sbl0nrmyl.execute-api.eu-west-2.amazonaws.com/prod"
+  },
+  cognito: {
+    REGION: "eu-west-2",
+    USER_POOL_ID: "eu-west-2_SXqAapG4k",
+    APP_CLIENT_ID: "3jqfs1s053m213av8iidjo0osb",
+    IDENTITY_POOL_ID: "eu-west-2_SXqAapG4k"
+  }
+};
